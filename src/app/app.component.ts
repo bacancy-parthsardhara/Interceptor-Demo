ac4faa33f62b0b134149499793fb24405cda035f
vijay.kalathiya <vijay.kalathiya@bacancytechnology.com>
2019-11-28 15:52:16 +0530

    Integration of interceptor in angular application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { LoaderService } from './loader.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Interceptor';
