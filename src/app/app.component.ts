5dfc90e26cd3c75728cfb471eb15209e53d87feb
vijay.kalathiya <vijay.kalathiya@bacancytechnology.com>
2019-11-28 15:49:21 +0530

    Integration of interceptor in angular application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LoaderService } from './loader.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Interceptor-Demo';
+  title = 'Interceptor';
+  public loading = false;
+
+
+  public items: any = [];
+
+  constructor(private http: HttpClient,
+    private loaderService: LoaderService,
+  ) { }
+
+  ngOnInit(): void {
+    this.loaderService
+    .getSpinnerObs().subscribe(val => {
+      this.loading = val;
+    })
+  }
+  public clickAPI() {
+    this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe((result) => {
+      console.log('res', result);
+      this.items = result;
+    });
+  }
+
+  public clear() {
+    this.items = [];
+  }
 }
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/Interceptor-Demo'),
+      dir: require('path').join(__dirname, '../coverage/Interceptor'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
